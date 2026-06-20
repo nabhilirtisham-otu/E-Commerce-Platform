b85b9251ded4c72b20876e1f2f766dd8d75b9040
Nabhil Irtisham <nabhil.irtisham@gmail.com>
2026-06-20 16:30:14 -0400

    Start creating page layout

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>                   //core wrapper component of react-router-dom package
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>                  //syncs React app UI with browser address bar
+);
