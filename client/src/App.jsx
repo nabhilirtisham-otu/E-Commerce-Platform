ec3e35166976288e689dbd8ac182764d42527625
Nabhil Irtisham <nabhil.irtisham@gmail.com>
2026-06-20 16:12:43 -0400

    Installing dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -115,6 +116,9 @@ function App() {
 
       <div className="ticks"></div>
       <section id="spacer"></section>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
     </>
   )
 }
